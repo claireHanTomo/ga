e85dcafe67d37ffdc54ecf2e031f0c64613115d6
han.start <50059127+clairehanstart@users.noreply.github.com>
2019-11-22 10:32:25 -0500

    Update ga.py

--- ga.py
+++ ga.py
@@ -6,12 +6,13 @@
 
 import sys
 from random import random
+from random import randrange
 
 # constants
 MAX = 120
 BOX_WEIGHT = [20, 30, 60, 90, 50, 70, 30]
 IMPORTANCE = [6, 5, 8, 7, 6, 9, 4]
-'''
+
 # Return an array of 8 random individual
 def initial():
     pop = [None] * 8
@@ -25,8 +26,8 @@ def initial():
                 genome[j] = False
         pop[i] = genome
     return pop
-'''
 
+'''
 # fake initial function to help us test the code
 def initial():
     pop = []
@@ -49,6 +50,7 @@ def initial():
     pop.append(genome8)
 
     return pop
+'''
 
 # Takes a genome
 # Returns the sum of importance, returns 0 if the weight is greater than max weight
@@ -69,19 +71,41 @@ def fitness(genome):
 
 # Takes a population
 # Returns a population with half size of the original population
-def random_select(population):
+def random_selection(population):
     # record population who survivel from cull
     cull = []
 
     # an array of survival probability for each genome in population
     survival_rate = []
+
+    # an array of fitness value for each genome in population
+    fit = []
+
+    for i in range(8):
+        imp_value = fitness(population[i])
+        fit.append(imp_value)
+
     # sum importance of all the genomes in population
     sum_val = 0
-    for genome in population:
-        sum_val = sum_val + fitness(genome)
-    # calculate survival rate based on fitness function
     for i in range(8):
-        survival_rate.append(round(fitness(population[i])/sum_val, 4)) 
+        sum_val = sum_val + fit[i]
+
+    # calculate survival rate based on sum importance
+    if sum_val != 0:
+        for i in range(8):
+            survival_rate.append(round(fitness(population[i])/sum_val, 4))
+    else: # None of them is good genome
+        cull.append(population[0])
+        cull.append(population[1])
+        cull.append(population[2])
+        cull.append(population[3])
+        return cull
+
+    # Even if the genome's value is 0, its genome still has a small probability to survive
+    # This is good for keep the variety of gene pool 
+    for i in range(8):
+        if survival_rate[i] == 0:
+            survival_rate[i] = 0.0001
 
     # choose 4 qualified genome and cull the rest of them
     for times in range(4):
@@ -94,13 +118,11 @@ def random_select(population):
             if survival_rate[i] > 0:
                 cumul_rate[i] = survival_rate[i] + cml
                 cml += survival_rate[i]
-            elif survival_rate[i] == 0:
-                # Even if the genome's value is 0 its genome still has a small probability to survive
-                # This is good for keep the variety of our gene pool 
-                cumul_rate[i] = 0.00001
-            elif survival_rate[i] == -1:
-                # the genome is already chosed
+            elif survival_rate[i] == -1: # the genome is already chosed
                 cumul_rate[i] = 0
+            else:
+                print ("error! survival_rate[i] is " + str(survival_rate[i]))
+
         # print ("the cumulative probabilites are:")
         # print (cumul_rate)
 
@@ -122,40 +144,75 @@ def random_select(population):
         for rate in survival_rate:
             if rate != -1:
                 sum_val += rate
+
         for i in range(8):
             if survival_rate[i] != -1:
                 survival_rate[i] = round(survival_rate[i]/sum_val, 4)
-
     # end of half cull
 
-    for genome in cull:
-        print_genome(genome)
+    #for genome in cull:
+    #    print_genome(genome)
 
     return cull
 
 
 
-'''
+
 # Takes two genomes
 # Returns a cross-over result of parents
 # cross-over the genome 
-def reproduce(mom, dad):
-'''
+def reproduce(moms, dads):
+
+    child = [None] * 7
+    # location of cross point
+    # for a genome with length 7, there are 6 candidate cross point
+    site = randrange(5) + 1
+
+    #choose mom
+    mom_index = randrange(3)
+    #choose dad
+    dad_index = randrange(3)
+
+    mom = moms[mom_index]
+    dad = dads[dad_index]
+
+    for i in range(0, site):
+        child[i] = mom[i]
+    for i in range(site, 7):
+        child[i] = dad[i]
+
+    #print ("mom:")
+    #print_genome(mom)
+    #print ("dad:")
+    #print_genome(dad)
+    #print (site)
+
+    return child
+
+
+
 
 # Takes a genome
 # Returns the mutation of it
 def mutate(genome):
-    pos = int(ranodm() * 7)
+    pos = int(random() * 7)
     genome[pos] = not genome[pos]
     return genome
 
-'''
-# Returns true if fit enough or enough time has elapsed
-def enough()
 
 # Takes a population and return the best one of them
 def best(population):
-'''
+    best_index = 0
+    best_result = fitness(population[0])
+    for i in range(1, 8):
+        imp = fitness(population[i])
+        if imp > best_result:
+            best_index = i
+            best_result = imp
+
+    return population[best_index]
+
+
 # Takes a genome
 # Print out what box we take based on the assignment in genome
 def print_genome(genome):
@@ -168,7 +225,8 @@ def print_genome(genome):
 # generates and search the best solution using genetic algorithm
 def ga(population):
 
-    while(not enough()):
+    timer = 0;
+    while(timer < 100):
         new_population = []
         for i in range(8):
             x = random_selection(population)
@@ -178,26 +236,25 @@ def ga(population):
             num = random()
             if num <= 0.1:
                 child = mutate(child)
-                new_population.append(child)
+                # print ("mutation is happening!")
+            new_population.append(child)
+
         population = new_population
 
-    return best(population)
+        timer += 1
 
+    return best(population)
 
 #run program
 print ("Maximum weight of the Backpack: 120")
 print ("Weight(Importance) of Boxes we can choose")
 print ("20(6) 30(5) 60(8) 90(7) 50(6) 70(9) 30(4)")
-print ("finding the best solution using genetic algorithm...")
+print ("\n")
 
 # generate initial population
 population = initial()
 
-#for genome in population:
-#    print_genome (genome)
-
-
-
+print ("finding the best solution using genetic algorithm...")
 best_population = ga(population)
-#print_genome(best_population)
+print_genome(best_population)
 
